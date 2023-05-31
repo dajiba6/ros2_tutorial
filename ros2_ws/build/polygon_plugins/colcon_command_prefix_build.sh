860287ecf4c2146a6450eb73bbb9d6f9f3d47cfb
chaoyining <chao.yn@foxmail.com>
2023-05-31 17:57:04 +0800

    ros2 plugins tutorial

---
+++ ros2_ws/build/polygon_plugins/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/cyn/ros2/ros2_ws/install/polygon_base/share/polygon_base/package.sh"
---
+++ ros2_ws/src/build/polygon_plugins/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/cyn/ros2/ros2_ws/src/install/polygon_base/share/polygon_base/package.sh"
